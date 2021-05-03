2190a161a675a40445cb85cda0b5223911d7eb5e
Monica Powell <monica@aboutmonica.com>
2021-05-03 07:34:56 -0400

    add abbv. version

--- roundBasedOnValue/index.js
+++ roundBasedOnValue/index.js
@@ -1,5 +1,7 @@
-function round(num) {
-  return num > 0 ? Math.floor(num) : Math.ceil(num);
-}
+const conciseRound = (num) => ~~num;
+const round = (num) => (num > 0 ? Math.floor(num) : Math.ceil(num));
 
-[1.7, -2.1, 500.4, -369.5, 150, -350].forEach((num) => console.log(round(num)));
+[1.7, -2.1, 500.4, -369.5, 150, -350].forEach((num) => {
+  console.log(conciseRound(num));
+  console.log(round(num));
+});
